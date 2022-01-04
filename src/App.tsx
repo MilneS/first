65fc9f1e3ebc1bb883e43bfb1121181f40a89f86
Samira Milne <milne.samira@GMAIL.COM>
2022-01-04 16:02:41 -0600

    styled button, main comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
-import Login from './comps/Login'
-import Signup from './comps/Signup'
+import "./App.css";
+import Button from "@mui/material/Button";
+import Arrow from "@material-ui/icons/ArrowForwardIos";
+import Main from './comps/Main'
+import {useState} from 'react'
 
 function App() {
-  return (
-    <div >
-      <h1>Material UI project.</h1>
-      {/* <Signup/>
-      <Login/> */}
+  const [showMain, setShowMain]= useState(false)
 
+  const mainHandler=()=>{
+    setShowMain(true)
+  }
+  return (
+    <div className="app">
+      {!showMain &&
+      <Button
+        endIcon={<Arrow />}
+        variant="contained"
+        color="success"
+        size="large"
+        style={{
+          fontSize: 22,
+          textTransform: "capitalize",
+          backgroundColor: "darkcyan",
+          margin: "250px",
+        }}
+        onClick={mainHandler}
+      >
+        Enter
+      </Button>
+      }
+      {showMain && <Main/>}
     </div>
   );
 }
