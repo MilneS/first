f32bd633fdb357b4728e07bf70b18957c0b0b915
Samira Milne <milne.samira@GMAIL.COM>
2022-01-05 09:54:50 -0600

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
 import "./App.css";
 import Button from "@mui/material/Button";
 import Arrow from "@material-ui/icons/ArrowForwardIos";
-import Main from './comps/Main'
-import {useState} from 'react'
+import Main from "./comps/Main";
+import { useState } from "react";
 
 function App() {
-  const [showMain, setShowMain]= useState(false)
+  const [showMain, setShowMain] = useState(false);
 
-  const mainHandler=()=>{
-    setShowMain(true)
-  }
+  const mainHandler = () => {
+    setShowMain(true);
+  };
   return (
     <div className="app">
-      {!showMain &&
-      <Button
-        endIcon={<Arrow />}
-        variant="contained"
-        color="success"
-        size="large"
-        style={{
-          fontSize: 22,
-          textTransform: "capitalize",
-          backgroundColor: "darkcyan",
-          margin: "250px",
-        }}
-        onClick={mainHandler}
-      >
-        Enter
-      </Button>
-      }
-      {showMain && <Main/>}
+      {!showMain && (
+        <div className="home">
+          <div className="titleCont">
+            <h1 className="title">Visit the Bakery</h1>
+          </div>
+          <Button
+            endIcon={<Arrow />}
+            variant="contained"
+            size="large"
+            style={{
+              fontSize: "20px",
+              textTransform: "capitalize",
+              backgroundColor: "#c72057",
+              marginTop: "160px",
+              marginLeft: "550px",
+            }}
+            onClick={mainHandler}
+          >
+            Enter
+          </Button>
+        </div>
+      )}
+
+      {showMain && <Main />}
     </div>
   );
 }
