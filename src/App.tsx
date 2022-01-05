d94f650522407ff8b5995a2d3f9b83b2c341ea6b
Samira Milne <milne.samira@GMAIL.COM>
2022-01-05 11:20:01 -0600

    primary override

--- src/App.tsx
+++ src/App.tsx
@@ -3,40 +3,54 @@ import Button from "@mui/material/Button";
 import Arrow from "@material-ui/icons/ArrowForwardIos";
 import Main from "./comps/Main";
 import { useState } from "react";
+// import { ThemeProvider } from "@material-ui/core/styles";
+import { createTheme } from "@material-ui/core";
+import { ThemeProvider } from "@mui/system";
 
 function App() {
   const [showMain, setShowMain] = useState(false);
 
+  const theme: any = createTheme({
+    palette: {
+      primary: {
+        main: "#c72057",
+      },
+    },
+  });
+
   const mainHandler = () => {
     setShowMain(true);
   };
-  return (
-    <div className="app">
-      {!showMain && (
-        <div className="home">
-          <div className="titleCont">
-            <h1 className="title">Visit the Bakery</h1>
-          </div>
-          <Button
-            endIcon={<Arrow />}
-            variant="contained"
-            size="large"
-            style={{
-              fontSize: "20px",
-              textTransform: "capitalize",
-              backgroundColor: "#c72057",
-              marginTop: "160px",
-              marginLeft: "550px",
-            }}
-            onClick={mainHandler}
-          >
-            Enter
-          </Button>
-        </div>
-      )}
 
-      {showMain && <Main />}
-    </div>
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="app">
+        {!showMain && (
+          <div className="home">
+            <div className="titleCont">
+              <h1 className="title">Visit the Bakery</h1>
+            </div>
+            <Button
+              color="primary"
+              endIcon={<Arrow />}
+              variant="contained"
+              size="large"
+              style={{
+                fontSize: "20px",
+                textTransform: "capitalize",
+                marginTop: "160px",
+                marginLeft: "550px",
+              }}
+              onClick={mainHandler}
+            >
+              Enter
+            </Button>
+          </div>
+        )}
+
+        {showMain && <Main />}
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/themes/theme.js
@@ -0,0 +1,10 @@
+import { createTheme } from "@material-ui/core/styles";
+
+
+export const theme = createTheme({
+  myButton: {
+    fontSize: "20px",
+    textTransform: "capitalize",
+    backgroundColor: "#c72057",
+  },
+});
